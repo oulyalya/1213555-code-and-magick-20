5f72455d8790ea8efe6ea66852f645171f21d24f
Gelezka20160321\oulyalya <olya.debelaya@gmail.com>
2020-06-21 16:10:27 +0300

    Открытие/закрытие формы; настройка имени и цветов экипы персонажа

--- js/setup.js
+++ js/setup.js
@@ -3,13 +3,59 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+var MIN_NAME_LENGTH = 2;
+var MAX_NAME_LENGTH = 25;
 
 var userDialog = document.querySelector('.setup');
+var userDialogOpen = document.querySelector('.setup-open');
+var userDialogClose = document.querySelector('.setup-close');
+
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var wizards = [];
 
 var fragment = document.createDocumentFragment();
+
+
+// Открытие и закрытие формы
+var onPopupEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+userDialogOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+userDialogOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+userDialogClose.addEventListener('click', function () {
+  closePopup();
+});
+
+userDialogClose.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    closePopup();
+  }
+});
 
 // Функция для получения рандомного элемента массива
 var getRandomElement = function (arr) {
@@ -47,6 +93,54 @@ var renderWizards = function () {
 renderWizards();
 similarListElement.appendChild(fragment);
 
-// удалить в 4 модуле
-userDialog.classList.remove('hidden');
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+var form = document.querySelector('.setup-wizard-form');
+var coatColorInput = form.querySelector('[name="coat-color"]');
+var eyesColorInput = form.querySelector('[name="eyes-color"]');
+var fireballColorInput = form.querySelector('[name="fireball-color"]');
+
+var coat = document.querySelector('.wizard-coat');
+var eyes = document.querySelector('.wizard-eyes');
+var fireball = document.querySelector('.setup-fireball-wrap');
+
+// Измененеие цвета элементов
+var changeColor = function (element, colorsArray, elementInput) {
+  var randomColor = getRandomElement(colorsArray);
+  element.style.fill = randomColor;
+  element.style.backgroundColor = randomColor;
+  elementInput.value = randomColor;
+};
+
+eyes.addEventListener('click', function () {
+  changeColor(eyes, EYES_COLORS, eyesColorInput);
+});
+
+coat.addEventListener('click', function () {
+  changeColor(coat, COAT_COLORS, coatColorInput);
+});
+
+fireball.addEventListener('click', function () {
+  changeColor(fireball, FIREBALL_COLORS, fireballColorInput);
+});
+
+// Валидация форма
+var userNameInput = form.querySelector('.setup-user-name');
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function () {
+  var valueLength = userNameInput.value.length;
+  form.reportValidity();
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
